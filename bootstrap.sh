ced034bdf6dc9e1642b052fcee7e25ad42444bb8
Jibidus <jibidus@gmail.com>
2020-05-22 20:13:23 +0200

    Replace git-config by dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,10 @@
 /usr/bin/ruby bootstrap.rb
 
 # Git Config
-mkdir ~/Development/Perso
-cd ~/Development/Perso
-git clone git@github.com:jibidus/git-config.git
-cd git-config
-ln -s ~/Development/Perso/git-config/.gitconfig ~/.gitconfig
+mkdir -p ~/Development
+cd ~/Development
+git clone git@github.com:jibidus/dotfiles.git
+ln -s ~/Development/dotfiles/.gitconfig ~/.gitconfig
 
 # TODO See https://github.com/eduncan911/dotfiles
 # TODO COMPLETE with https://github.com/lra/mackup
