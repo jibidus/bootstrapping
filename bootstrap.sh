95f0eef6a9a3c86d6da430b4e5365ec4cb692ed5
Jibidus <jibidus@gmail.com>
2025-11-10 16:34:48 +0100

    Do not use rbenv to install bootstrapping requirements

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,13 +6,7 @@ brew upgrade
 brew cleanup
 
 # Install ruby requirements (open3 gem)
-brew install rbenv ruby-build
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zprofile
-echo 'eval "$(rbenv init -)"' >> ~/.zprofile
-source ~/.zprofile
-rbenv install 2.6.8
-rbenv global 2.6.8
-rbenv rehash
+brew install ruby
 gem install open3
 
 /usr/bin/ruby bootstrap.rb
